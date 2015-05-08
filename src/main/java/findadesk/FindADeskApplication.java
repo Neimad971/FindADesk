eaaf94d2d821978f8e17bac38a9f5d6423c3211e
neimad971 <damientalbot26@gmail.com>
2015-05-08 12:10:21 +0200

    initial commit

---
+++ src/main/java/findadesk/FindADeskApplication.java
@@ -0,0 +1,12 @@
+package findadesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindADeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindADeskApplication.class, args);
+    }
+}
---
+++ src/test/java/findadesk/FindADeskApplicationTests.java
@@ -0,0 +1,16 @@
+package findadesk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FindADeskApplication.class)
+public class FindADeskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
