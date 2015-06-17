bba3d7f6b0c17d7cd6f8254cd8cf5a4a751a2c12
hussam <chaudhry.hussam@gmail.com>
2015-06-17 12:03:05 +0200

    Added methods to get workspaces from search for listing

--- src/main/java/fr/esgi/findadesk/repository/IWorkspaceRepository.java
+++ src/main/java/fr/esgi/findadesk/repository/IWorkspaceRepository.java
@@ -2,17 +2,29 @@ package fr.esgi.findadesk.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.esgi.findadesk.domain.Workspace;
 
-
 @RepositoryRestResource
-public interface IWorkspaceRepository extends CrudRepository<Workspace, Integer>
-{
+public interface IWorkspaceRepository extends
+		CrudRepository<Workspace, Integer> {
 	
 	@Transactional
 	public List<Workspace> findAll();
+
+	@Query(value = "SELECT city FROM workspace GROUP BY city")
+	public List<Workspace> findWorkspaceLocations();
+
+	
+	@Query(value = "SELECT w FROM workspace w WHERE w.typeId = :type AND w.city = :city AND w.seatsNumber = :seatsNumber "
+			+ "AND w.price BETWEEN :minPrice AND :maxPrice")
+	public List<Workspace> findWorkspacesForListing(@Param("type") Long type,
+			@Param("city") String city, @Param("seatsNumber") int seatsNumber,
+			@Param("minPrice") double minPrice,
+			@Param("maxPrice") double maxPrice);
 }
--- src/main/java/fr/esgi/findadesk/web/WorkspaceController.java
+++ src/main/java/fr/esgi/findadesk/web/WorkspaceController.java
@@ -3,6 +3,7 @@ package fr.esgi.findadesk.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,20 @@ import fr.esgi.findadesk.domain.Workspace;
 import fr.esgi.findadesk.repository.IWorkspaceRepository;
 
 @RestController
-public class WorkspaceController 
-{
+public class WorkspaceController {
 
 	@Autowired
 	private IWorkspaceRepository workSpaceDao;
-	
+
 	@RequestMapping("/workspaces")
-    public List<Workspace> getAllUsers() 
-    {
+	public List<Workspace> getAllUsers() {
 		return workSpaceDao.findAll();
-    }
+	}
+
+	@RequestMapping("/workspacesListing/{typeId}/{city}/{seats}/{minPrice}/{maxPrice}")
+	public List<Workspace> getWorkspacesForListing(@PathVariable Long typeId,
+			@PathVariable String city, @PathVariable int seats,
+			@PathVariable double minPrice, @PathVariable double maxPrice) {
+		return workSpaceDao.findWorkspacesForListing(typeId, city, seats, minPrice, maxPrice);
+	}
 }
